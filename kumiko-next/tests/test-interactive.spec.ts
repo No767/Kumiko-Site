35c0e6a480bdf0465224212b253d8c0482ba8355
No767 <73260931+No767@users.noreply.github.com>
2022-07-03 01:57:59 -0700

    Format Playwright Tests

--- kumiko-next/tests/test-interactive.spec.ts
+++ kumiko-next/tests/test-interactive.spec.ts
@@ -1,22 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Check Headers', () => {
+test.describe("Check Headers", () => {
   test.beforeEach(async ({ page }) => {
     // Go to the starting url before each test.
-    await page.goto('http://127.0.0.1:3000'); // basically use localhost for now, but in prod use the actual site
+    await page.goto("http://127.0.0.1:3000"); // basically use localhost for now, but in prod use the actual site
   });
 
-  
-  test('Check Login Button', async ({ page }) => {
+  test("Check Login Button", async ({ page }) => {
     // Assertions use the expect API.
 
-    page.click('text=Login');
-    await expect(page).toHaveURL('http://127.0.0.1:3000/login');
+    page.click("text=Login");
+    await expect(page).toHaveURL("http://127.0.0.1:3000/login");
   });
 
-  test('Check GitHub Button', async ({ page }) => {
-    page.click('text=Github');
-    await expect(page).toHaveURL('https://github.com/No767/Kumiko');
-
+  test("Check GitHub Button", async ({ page }) => {
+    page.click("text=Github");
+    await expect(page).toHaveURL("https://github.com/No767/Kumiko");
   });
 });
