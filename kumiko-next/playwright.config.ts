3f41cabce0aad8a3ec327e6a04bf129c3e2a8baa
No767 <73260931+No767@users.noreply.github.com>
2022-07-03 01:56:32 -0700

    Add Playwright Tests

---
+++ kumiko-next/playwright.config.ts
@@ -0,0 +1,39 @@
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    // {
+    //     name: 'Pixel 4',
+    //     use: {
+    //       browserName: 'chromium',
+    //       ...devices['Pixel 4'],
+    //     },
+    //   },
+  
+    //   {
+    //     name: 'iPhone 11',
+    //     use: {
+    //       browserName: 'webkit',
+    //       ...devices['iPhone 11'],
+    //     },
+    //   },
+  ],
+};
+export default config;
---
+++ kumiko-next/tests/test-interactive.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Check Headers', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto('http://127.0.0.1:3000'); // basically use localhost for now, but in prod use the actual site
+  });
+
+  
+  test('Check Login Button', async ({ page }) => {
+    // Assertions use the expect API.
+
+    page.click('text=Login');
+    await expect(page).toHaveURL('http://127.0.0.1:3000/login');
+  });
+
+  test('Check GitHub Button', async ({ page }) => {
+    page.click('text=Github');
+    await expect(page).toHaveURL('https://github.com/No767/Kumiko');
+
+  });
+});
